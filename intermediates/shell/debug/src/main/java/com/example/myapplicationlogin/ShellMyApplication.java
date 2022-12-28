5ef16ed054d9acd170219c3e16b60256f332659f
ker-zjc <116044429+ker-zjc@users.noreply.github.com>
2022-12-28 23:59:36 +0800

    Add files via upload

---
+++ intermediates/shell/debug/src/main/java/com/example/myapplicationlogin/ShellMyApplication.java
@@ -0,0 +1,10 @@
+package com.example.myapplicationlogin;
+
+import ohos.abilityshell.HarmonyApplication;
+
+public class ShellMyApplication extends HarmonyApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ intermediates/shell/debug/src/main/java/com/example/myapplicationlogin/WelcomeAbilityShellActivity.java
@@ -0,0 +1,11 @@
+package com.example.myapplicationlogin;
+
+import android.os.Bundle;
+import ohos.abilityshell.AbilityShellActivity;
+
+public class WelcomeAbilityShellActivity extends AbilityShellActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
